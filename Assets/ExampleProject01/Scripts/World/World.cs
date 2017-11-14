fdeaa1eb9746e59cb279e828370c6e302a51a746
Young-PC\Young <376693007@qq.com>
2017-11-14 22:36:56 +0800

    提交 ExampleProject01

---
+++ Assets/ExampleProject01/Scripts/World/World.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// A world is defined as a group of tiles or grids, and extra info on each grid.
+/// </summary>
+public class World : MonoBehaviour
+{
+    /// <summary>
+    /// Or tile size, on the xz plane
+    /// </summary>
+    public Vector2 gridSize = new Vector2(1, 1);
+    public Vector2 gridNum = new Vector2(100, 100);
+    
+
+}
---
+++ Assets/ExampleProject01/Scripts/World/WorldAssetDefine.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldAssetDefine : ScriptableObject
+{
+    #region attributes
+    public float density;
+
+    #endregion
+
+    public List<Object> prefabs;
+}
